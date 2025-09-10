3ac42d8c10026c2c1888a65691d404a593f1cfeb
Rama <ravindukalhara215@gmail.com>
2025-09-10 22:04:13 +0530

    change if statement

--- src/LW_01/Q01.java
+++ src/LW_01/Q01.java
@@ -1,9 +1,27 @@
 package LW_01;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Q01 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter an odd-length word: ");
+        String word = scanner.nextLine();
+
+
+        if (word.length() % 2 == 0) {
+            System.out.println("Try again. Please enter odd-length word .");
+        } else {
+
+            int middleIndex = word.length() / 2;
+
+
+            char middleChar = word.charAt(middleIndex);
+            System.out.println("The middle character is: " + middleChar);
+        }
+
+        scanner.close();
     }
 }
