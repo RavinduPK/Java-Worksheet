1d6a0f587f2a37f5d6078eccff4d918c8e951cec
Rama <ravindukalhara215@gmail.com>
2025-09-11 00:17:44 +0530

    create Q04

---
+++ src/LW_01/Q04.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Q04 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("My First Frame");
+
+
+        frame.setSize(300, 200);
+
+
+        frame.setLocation(50, 100);
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        frame.setVisible(true);
+    }
+}
