a357c941cc4765052dcf4daeae585c16584c6310
Rama <ravindukalhara215@gmail.com>
2025-09-11 14:17:55 +0530

    create Q06

---
+++ src/LW_02/Q06.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Q06 {
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+
+        String formattedDate = today.format(formatter);
+        System.out.println("Today date :" + formattedDate);
+    }
+}
