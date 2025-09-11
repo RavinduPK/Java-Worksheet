4a6f582a303f5cfcb28dd8b6eb9220c44aed052a
Rama <ravindukalhara215@gmail.com>
2025-09-11 14:23:43 +0530

    create Q07 change in Q06

--- src/LW_02/Q07.java
+++ src/LW_02/Q07.java
@@ -3,13 +3,13 @@ package LW_02;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class Q06 {
+public class Q07 {
     public static void main(String[] args) {
 
         LocalDate today = LocalDate.now();
 
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE , MMMM d , yyyy");
 
         String formattedDate = today.format(formatter);
         System.out.println("Today date :" + formattedDate);
